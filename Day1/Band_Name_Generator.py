dac671bf6dd390554c9fed259a54cacd9cd854cc
Somesh Raut <someshraut122@gmail.com>
2025-07-21 19:59:24 +0530

    initial Commit

---
+++ Day1/Band_Name_Generator.py
@@ -0,0 +1,5 @@
+print("Welcoome to Band-name Generator!!!")
+CityName = input("Enter Your City Name\n")
+PetName = input("enter Your Pet Name\n")
+
+print("Your Band-Name is " + CityName + " "+ PetName)
